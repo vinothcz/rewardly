f018e05569bad582c57a1b203b636cf8e4025b6f
unknown <rajanz@RAJANZ-PC.abcoind.local>
2016-02-10 18:03:16 +0530

    Integrated ui with api

--- Rewardly-UI/app/js/main.js
+++ Rewardly-UI/app/js/main.js
@@ -39,6 +39,47 @@ $(document).ready(function(){
 		$('.badge-label').text(item.name);
 	});
 
+	$('.suggestr').suggestr();
 
+	$.ajax({
+		type: 'GET',
+		// data: JSON.stringify(data),
+	    contentType: 'application/json',
+	    url: 'http://localhost:3007/api/feeds',						
+	    success: function(data) {
+	        console.log('Success: getfeeds')
+	    },
+	    error: function (xhr, status, error) {
+            console.log('Error: ' + error.message);
+        },
+	});
 
+});
+
+$("#reward-btn").click(function(){
+	var rewardtitle = $('.badge-label').text();
+	var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "http://localhost:3007/api/rewards",
+	  "method": "POST",
+	  "headers": {
+	    "content-type": "application/json",
+	    "cache-control": "no-cache",
+	    "postman-token": "efc5c122-d5c0-1c06-0962-f63bdabc36ea"
+	  },
+	  "processData": false,
+	  "data": JSON.stringify(
+	  	{ 
+	  		"type": rewardtitle,
+  			"points": "20",
+  			"user_recieved": "56bb18622f7f054d5d1be781",
+  			"user_awarded": "56bb1b762f7f054d5d1be782"
+	  	})
+
+	}
+
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	});
 });
